f06f659c47a5ddb09305e84b75fc9215f45d0e63
Sharaj Panwar <ijz121@my.utsa.edu>
2019-03-20 11:36:46 -0500

    Update Pre_processing_link.py

--- pre_processing/Pre_processing_link.py
+++ pre_processing/Pre_processing_link.py
@@ -1,4 +1,4 @@
 
-# The pre-processing steps can be found in below link, published by co-author Mr. Arun DasL:
+# The pre-processing steps can be found in link below, published by co-author Mr. Arun Das:
 https://github.com/arundasan91/MagnaTagATune
   
